125e28503e22b9a9bd9d451c929cc3ab42338612
AnCheTeng <ancheteng@AnCheTengde-MacBook-Pro.local>
2016-03-20 02:16:59 +0800

    add api

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
 
   setInterval(function() {
-    console.log("Hi");
-    // Call the API
-    // API: list
-    // API: renew
-    // API: analysis
-  }, 1500);
+    // Call the API------
+    // API: /hospitalvm/list
+    // API: /hospitalvm/renew
+    // API: /hospitalvm/analysis
+    var list_api = "/hospitalvm/list";
+    var renew_api = "/hospitalvm/renew";
+    var analysis_api = "/hospitalvm/analysis";
+
+    $.get(list_api, (response)=>{
+      var hospital_list = response[0]+"，"+response[1]+"，"+response[2];
+      $("#hospital_list").text(hospital_list);
+    });
+
+    // $.get(renew_api, ()=>{});
+    //
+    // $.get(analysis_api, (response)=>{
+    //
+    // });
+
+  }, 1000);
+
 
   $("button").on("click", function() {
     var value = $("input").val();
-    if(ValidateIPaddress(value)==true){
-      // Load the API
-      // API: load/value
-      console.log("Yo");
-      console.log(value);
-    }
-  })
+    // Load the API------
+    // API: load/value
+    var complete_api = "/hospitalvm/load/" + value
+    console.log(complete_api);
+    // $.get(complete_api, ()=>{});
+
+  });
 
 
 
-})
+});
