5209b1487f51324ca3c80e8c4679861576532fd3
AnCheTeng <ancheteng@AnCheTengde-MacBook-Pro.local>
2016-03-20 01:57:56 +0800

    FrontEnd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
 
+  setInterval(function() {
+    console.log("Hi");
+    // Call the API
+    // API: list
+    // API: renew
+    // API: analysis
+  }, 1500);
+
+  $("button").on("click", function() {
+    var value = $("input").val();
+    if(ValidateIPaddress(value)==true){
+      // Load the API
+      // API: load/value
+      console.log("Yo");
+      console.log(value);
+    }
+  })
+
+
+
+})
